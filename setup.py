a9b68f5872ebbcf0625f9824cbf954013ab26b3d
Michael Schwarz <michi.schwarz@gmail.com>
2020-02-28 20:09:16 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='filemaster',
+    version='0.1',
+    packages=['filemaster'],
+    entry_points=dict(
+        console_scripts=[]),
+    install_requires=[])
