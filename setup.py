7104564c2a51f65f6421012d0c94a4fa4c2aaed6
Michael Schwarz <michi.schwarz@gmail.com>
2020-03-14 19:59:20 +0100

    Migrate to setuptools_scm for version number

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
+name = 'filemaster'
+dependencies = []
+console_scripts = ['fm = filemaster.cli:entry_point']
 
 setuptools.setup(
-    name='filemaster',
-    version='0.1',
-    packages=['filemaster'],
-    entry_points=dict(
-        console_scripts=['fm = filemaster.cli:entry_point']),
-    install_requires=[])
+    name=name,
+    entry_points=dict(console_scripts=console_scripts),
+    install_requires=dependencies,
+    setup_requires=['setuptools>=42', 'wheel', 'setuptools_scm[toml]>=3.4'],
+    use_scm_version=True,
+    packages=setuptools.find_packages(exclude=['tests']))
+
