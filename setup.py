a15708cf1f69af64f439c99908329a4501235f6f
Michael Schwarz <michi.schwarz@gmail.com>
2020-03-14 19:59:47 +0100

    Add [dev] extra which installs development dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ import setuptools
 
 name = 'filemaster'
 dependencies = []
+dependencies_dev = ['pytest']
 console_scripts = ['fm = filemaster.cli:entry_point']
 
 setuptools.setup(
     name=name,
     entry_points=dict(console_scripts=console_scripts),
     install_requires=dependencies,
+    extras_require={'dev': dependencies_dev},
     setup_requires=['setuptools>=42', 'wheel', 'setuptools_scm[toml]>=3.4'],
     use_scm_version=True,
     packages=setuptools.find_packages(exclude=['tests']))
